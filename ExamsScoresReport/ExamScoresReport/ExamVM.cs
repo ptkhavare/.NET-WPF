e0ffbe24512b4dd8a5900714671a8721806e0f7f
Pranav Khavare <pranavkhavare@gmail.com>
2022-03-15 10:12:37 -0400

    Added Files to Jagged Array and ObservableCollection

---
+++ ExamsScoresReport/ExamScoresReport/ExamVM.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace ExamScoresReport
+{
+
+
+    internal class ExamVM : INotifyPropertyChanged
+    {
+        private const string filePath = "C:\\Users\\prana\\Workspace\\PROG8010\\ExamsScoresReport\\ExamScoresReport\\scores";
+
+        private string result = "Result here";
+
+        public string Result
+        {
+            get { return result; }
+            set { result = value; NotifyPropertyChanged(); }
+        }
+
+        static int numOfFiles = Directory.GetFiles(filePath).Length;
+
+        public ObservableCollection<StudentScore> studentScoresSection1 { get; set; } = new ObservableCollection<StudentScore>();
+        public ObservableCollection<StudentScore> studentScoresSection2 { get; set; } = new ObservableCollection<StudentScore>();
+        public ObservableCollection<StudentScore> studentScoresSection3 { get; set; } = new ObservableCollection<StudentScore>();
+
+        public void GenerateReport()
+        {
+            (bool isError, StudentScore[][] studentScores) = FilesToArray("filePath");
+
+            ObservableCollection<StudentScore>[] ss = new ObservableCollection<StudentScore>[]
+                            { studentScoresSection1,
+                              studentScoresSection2,
+                              studentScoresSection3 };
+
+            for (int i = 0; i < studentScores.Length; i++)
+            {
+                for (int j = 0; j < studentScores[i].Length; j++)
+                {
+                    ss[i].Add(studentScores[i][j]);
+                }
+            }
+
+        }
+
+        public (bool, StudentScore[][]) FilesToArray(string filePath)
+        {
+            string[] files = Directory.GetFiles("C:\\Users\\prana\\Workspace\\PROG8010\\ExamsScoresReport\\ExamScoresReport\\scores");
+
+            StudentScore[][] studentScores = new StudentScore[numOfFiles][];
+
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string text;
+                try
+                {
+                    text = File.ReadAllText(files[i]);
+                }
+                catch
+                {
+                    return (true, studentScores);
+                }
+
+                string[] lines = text.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                studentScores[i] = new StudentScore[lines.Length];
+                int j;
+                for (j = 0; j < lines.Length; j++)
+                {
+                    StudentScore studentScore = new StudentScore(j, lines[j], i);
+                    studentScores[i][j] = studentScore;
+                }
+            }
+
+            return (false, studentScores);
+        }
+
+        #region Property Changed
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
---
+++ ExamsScoresReport/ExamScoresReport/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExamScoresReport
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly ExamVM examVM = new();
+
+        public MainWindow()
+        {
+            DataContext = examVM;
+            InitializeComponent();
+            
+        }
+
+        public void GenerateReport(object sender, RoutedEventArgs e)
+        {
+            examVM.GenerateReport();
+        }
+
+    }
+
+
+}
